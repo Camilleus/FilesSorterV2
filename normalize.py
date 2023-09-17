029ddb2d360604dd6c347bd8b950a7000aa08037
CamilleusRex <camilleusrex@gmail.com>
2023-09-17 13:55:11 +0200

    separeted normalize function

---
+++ normalize.py
@@ -0,0 +1,10 @@
+def normalize(input_str):
+    transliteration_dict = {
+        'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n',
+        'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z',
+        'Ą': 'A', 'Ć': 'C', 'Ę': 'E', 'Ł': 'L', 'Ń': 'N',
+        'Ó': 'O', 'Ś': 'S', 'Ź': 'Z', 'Ż': 'Z'
+    }
+    normalized_str = ''.join(transliteration_dict.get(char, char) for char in input_str)
+    normalized_str = ''.join(char if char.isalnum() else '_' for char in normalized_str)
+    return normalized_str
