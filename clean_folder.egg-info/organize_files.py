57438934b6f2de536cda66262873549a1a7a5675
CamilleusRex <camilleusrex@gmail.com>
2023-11-19 17:28:31 +0100

    added threading

--- clean_folder.egg-info/organize_files.py
+++ clean_folder.egg-info/organize_files.py
@@ -1,45 +1,31 @@
 import os
 import shutil
 from normalize import normalize
-
-def get_extension(filename):
-    return os.path.splitext(filename)[1].lower()
-
-def move_to_category(file_path, category_folder):
-    try:
-        shutil.move(file_path, os.path.join(category_folder, os.path.basename(file_path)))
-    except Exception as e:
-        print(f"Error moving file: {e}")
+from concurrent.futures import ThreadPoolExecutor
 
 def organize_files(src_folder, dest_folder, file_extensions):
     try:
-        unknown_folder = os.path.join(dest_folder, 'Unknown')
-        image_extensions = ('.jpeg', '.jpg', '.png', '.svg')
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
 
-        for root, _, files in os.walk(src_folder):
-            for filename in files:
-                extension = get_extension(filename)
-                found = False
+        with ThreadPoolExecutor() as executor:
+            for root, _, files in os.walk(src_folder):
+                for filename in files:
+                    file_path = os.path.join(root, filename)
+                    executor.submit(organize_file, file_path, dest_folder, file_extensions)
 
-                for category, extensions in file_extensions.items():
-                    category_folder = os.path.join(dest_folder, category)
-
-                    if extension in extensions:
-                        if extension in image_extensions:
-                            destination_folder = os.path.join(dest_folder, 'Zdjecia')
-                            os.makedirs(destination_folder, exist_ok=True)
-                            new_file_path = os.path.join(destination_folder, normalize(filename))
-                            move_to_category(os.path.join(root, filename), destination_folder)
-
-                        if not os.path.exists(category_folder):
-                            os.makedirs(category_folder)
-                        move_to_category(os.path.join(root, filename), category_folder)
-                        found = True
-                        break
-
-                if not found:
-                    if not os.path.exists(unknown_folder):
-                        os.makedirs(unknown_folder)
-                    move_to_category(os.path.join(root, filename), unknown_folder)
     except Exception as e:
         print(f"Error organizing files: {e}")
+
+def organize_file(file_path, dest_folder, file_extensions):
+    try:
+        extension = get_extension(filename)
+        for category, extensions in file_extensions.items():
+            if extension in extensions:
+                category_folder = os.path.join(dest_folder, category)
+                if not os.path.exists(category_folder):
+                    os.makedirs(category_folder)
+                move_to_category(file_path, category_folder)
+                break
+    except Exception as e:
+        print(f"Error organizing file {file_path}: {e}")
